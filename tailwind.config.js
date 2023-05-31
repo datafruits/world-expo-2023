3a3d80a57c7a0521de9bfa8d7912c8176ce0c246
Tony Miller <mcfiredrill@gmail.com>
2023-05-30 23:32:48 -0700

    fixed tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,8 @@
 'use strict';
 // located in <app root>/config/tailwind/
 
-const path = require('path');
-
-const appRoot = path.join(__dirname, '../../');
-const appEntry = path.join(appRoot, 'app');
-const relevantFilesGlob = '**/*.{html,js,ts,hbs,gjs,gts}';
-
 module.exports = {
-  content: [path.join(appEntry, relevantFilesGlob)],
+  content: ['./app/**/*.{gjs,gts,hbs,html,js,ts}'],
   theme: {
     fontFamily: {
       debussy: ['debussy', 'sans-serif'],
