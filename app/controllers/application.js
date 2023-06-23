99a8e3113c95c793b9ec335d3f6770d8a40d2f0b
Dakota Clark <dakotamusic@gmail.com>
2023-06-23 00:23:45 -0400

    add sarahs flyer

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,8 +1,11 @@
 import Controller from '@ember/controller';
 import flyerUrl from '../assets/fruit_shower_xpo.jpg';
-
+import worldExpoFlyer from '../assets/worldexpo.png'
 export default class ApplicationController extends Controller {
   get flyer() {
     return flyerUrl;
+  }
+  get expo() {
+    return worldExpoFlyer;
   }
 }
