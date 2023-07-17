5c177bc693418e588afc20f363d7266d55c2ba76
Dakota Clark <dakotamusic@gmail.com>
2023-07-16 22:16:32 -0400

    add beamsprout flyer

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,12 +1,16 @@
 import Controller from '@ember/controller';
 import flyerUrl from '../assets/fruit_shower_xpo.jpg';
 import worldExpoFlyer from '../assets/worldexpo.png';
+import beamSproutFlyer from '../assets/WOLRD_EXPO.png';
 import wristBandz1 from '../assets/expo_bandz_1.png';
 import wristBandz2 from '../assets/expo_bandz_2.png';
 
 export default class ApplicationController extends Controller {
   get flyer() {
     return flyerUrl;
+  }
+  get beam() {
+    return beamSproutFlyer;
   }
   get expo() {
     return worldExpoFlyer;
